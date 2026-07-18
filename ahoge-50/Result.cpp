ef333bc89384c20e52b3e56af09cb3b4ef45d885
ireven <hensati11@gmail.com>
2026-07-18 18:34:24 +0900

    ストーリーテキストを少し追加

--- ahoge-50/Result.cpp
+++ ahoge-50/Result.cpp
@@ -38,14 +38,16 @@ void Result::draw()
 	if (context()->getScore()->getCurrentScore() <= 0)
 	{
 		DrawString(860, 150, "BadEnd", GetColor(25, 25, 225));
+		DrawString(225, 225, "投げたクリスタルで、通行人に怪我を負わせてしまい逮捕...", GetColor(25, 25, 225));
 
-		DrawRotaGraphF(init_screen_width * 0.5f, init_screen_height * 0.5f, 0.5, 0.0, badEndGraph_, true);
+		DrawRotaGraphF(init_screen_width * 0.5f, init_screen_height * 0.55f, 0.5, 0.0, badEndGraph_, true);
 	}
 	else
 	{
 		DrawString(850, 150, "HappyEnd", GetColor(225, 225, 25));
+		DrawString(200, 225, "たくさんの人々がクリスタルを受け取り、皆が幸せになった...!!", GetColor(225, 25, 25));
 
-		DrawRotaGraphF(init_screen_width * 0.5f, init_screen_height * 0.5f, 0.5, 0.0, happyEndGraph_, true);
+		DrawRotaGraphF(init_screen_width * 0.5f, init_screen_height * 0.55f, 0.5, 0.0, happyEndGraph_, true);
 	}
 
 	DrawString(600, UpDownPositionY(900), "press Enter to return title", GetColor(25, 25, 25));
